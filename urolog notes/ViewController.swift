781ae60ec76faa3993b9b36ece405c90882c6223
Joseph Clemente <jpclemente97@gmail.com>
2019-03-16 13:44:08 -0400

    Initial Commit

---
+++ urolog notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  urolog notes
+//
+//  Created by Joseph Clemente on 3/16/19.
+//  Copyright © 2019 Joseph Clemente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
